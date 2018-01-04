096e35a928183c18906fdd4c3cd7c302261cc38e
Sven Bendel <ubuntudroid@gmail.com>
2018-01-04 17:34:54 +0100

    Document TimeEntry

--- app/src/main/java/io/github/ubuntudroid/toggltracker/network/model/TimeEntry.kt
+++ app/src/main/java/io/github/ubuntudroid/toggltracker/network/model/TimeEntry.kt
@@ -7,10 +7,38 @@ data class TimeEntry(
 )
 
 data class Data(
+        /**
+         * time entry ID
+         */
         val id: Long = 0,
+
+        /**
+         * workspace ID
+         */
         val wid: Long = 0,
+
+        /**
+         * time entry start time
+         */
         val start: Date?,
+
+        /**
+         * time entry stop time, can be not set
+         */
         val stop: Date?,
+
+        /**
+         * time entry duration in seconds.
+         *
+         * If the time entry is currently running, the duration attribute contains a negative value,
+         * denoting the start of the time entry in seconds since epoch (Jan 1 1970). The correct
+         * duration can be calculated as current_time + duration, where current_time is the current
+         * time in seconds since epoch.
+         */
         val duration: Long = 0,
-        val description: String = ""
+
+        /**
+         * An optional task subscription
+         */
+        val description: String?
 )
